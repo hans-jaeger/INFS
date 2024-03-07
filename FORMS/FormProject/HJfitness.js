04764d5730830780af937baa098ba79c03f5f81d
Hans Jaeger <hansjaeger@a-66-231.resnet.andrews.edu>
2024-03-07 16:52:47 -0500

    I have fixed the display ratio on a regular laptop and have placed more comments on each file

--- FORMS/FormProject/HJfitness.js
+++ FORMS/FormProject/HJfitness.js
@@ -13,26 +13,34 @@ var weightValue ;
 var bmiFinal;
 var balance;
 var message;
-//Function Message
 
+/*
+Description: The function showMessage displays an alert message when the user selects a specific section from the select tag (specifically the id activityList). Whenever the user choices the following options an informational alert message will pop up: Sedentary, Active, Athlete. 
+*/
 
 function showMessage( ) {
+  //Grab the value 
    var selectedOption = document.getElementById("activityList").value;
+  
+   //If Section
    if (selectedOption == "Sedentary"){
     alert(  "Little physical activity. Spends most of the day sitting, reading, socializing, watching television, playing video games, and using the computer.")
 
 }
-if (selectedOption == "Active"){
+  if (selectedOption == "Active"){
     alert(  "Has a daily exercise routine, such as working out at a gym, going for a walk or jog, or doing some regular weightlifting")
 
 }
-if(selectedOption == "Athlete"){
+  if(selectedOption == "Athlete"){
     alert("Engages in vigorous sporting activities or training 3 – 4 hours a day 6 days a week.")
 }
   }
 
 
- 
+/*
+Description: The baseCalories function uses the information from the User Information and uses the information to calculate the base calories of a person. 
+The function uses the Date class (to set and grab the current date).
+*/
   function baseCalories() {
     // Get the birth date 
     const birthDateInput = document.getElementById('userBirthDate');
@@ -62,18 +70,21 @@ if(selectedOption == "Athlete"){
         valueCalories += 1.9 * (655 + (4.35 * weightValue) + (4.7 * heightValue) - (4.7 * age));
       }
     if (valueCalories > 0){
-
     
+
     alert("Your Base Calories: " + valueCalories);
     }
     else{
+      //Debug the values 
         console.log(valueCalories)
         alert("Please Input Values")
     }
   
   }
   
-
+/*
+Description: The function calculates the users BMI in which the information is derived from the user information section.  
+*/ 
 function bmiCalc(){
     //Declaring variables and storing them into variables
     height = document.getElementById("userHeight").value;
@@ -92,6 +103,10 @@ function bmiCalc(){
     
     console.log(bmiFinal)
  }
+ /*
+ Description: The function calcUsage calculates the calories and activities the user has taken throughout the day. It will either output if the user as consumed more or less than the given information. 
+ The message will be displayed to a textarea. 
+ */
  function calcUsage() {
     // Declare Variables:
     var activities = 0;
@@ -116,15 +131,21 @@ function bmiCalc(){
     var balance = consumed - baseCalories - activities;
     var message;
   
+    //If Else If Section
     if (balance > 0) {
       message = "You have consumed more calories!";
     } else if (balance < 0) {
       message = "You have burned more calories!";
-    } else {
-      message = "ERROR";
     }
-  
+    else if(balance == 0){
+      message = "You have consumed 0 calories";
+    } 
+    else if(activities ==0 && consumed == 0){
+      message = "ERROR: Please Select Values";
+    }
     document.getElementById("Results").value = message;
+
+    
     //For Debug Reasons
     console.log(message + " "+ balance)
   }
